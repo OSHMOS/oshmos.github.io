922784074937a14df86c3b70eb716f346a90d4a8
OSHMOS <ojames9707@gmail.com>
2022-04-22 12:36:33 +0900

    노마드 JS 챌린지 : 졸업작품 헤더바 만들기, 실시간 시계

---
+++ src/header.js
@@ -0,0 +1,22 @@
+colors = ['#2D8CD5', '#904EAD', '#EEBC11'];
+
+const header = document.querySelector('.header');
+const clock = document.querySelector('.clock');
+
+function changeBoxBgColor() {
+    const boxBgColor = colors[Math.floor(Math.random() * colors.length)];
+    header.style.background = `${boxBgColor}`;
+}
+
+function whatTimeIsIt() {
+    const date = new Date();
+    const hour = String(date.getHours()).padStart(2, '0');
+    const min = String(date.getMinutes()).padStart(2, '0');
+    const sec = String(date.getSeconds()).padStart(2, '0');
+    clock.innerHTML = `${hour}:${min}:${sec}`;
+}
+
+header.style.background = colors[0];
+whatTimeIsIt();
+setInterval(whatTimeIsIt, 1000);
+setInterval(changeBoxBgColor, 1000);
