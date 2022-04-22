86165a2bd7a129099e228f8486212f76214d690a
OSHMOS <ojames9707@gmail.com>
2022-04-22 17:56:06 +0900

    노마드 JS 챌린지 : 로그인해야 시계 나오는 걸로 바꾸기

--- src/login.js
+++ src/login.js
@@ -1,5 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const clock = document.querySelector('#clock');
+const header = document.querySelector('.header');
+const question = document.querySelector('#question');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
@@ -7,17 +10,27 @@ const USERNAME_KEY = "username";
 
 
 function onLogInSubmit(e) {
-    // e.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem("username", username);
 }
 
 function paintGreetings(username) {
-    greeting.innerHTML = `Hello ${username}`;
+    greeting.innerHTML = `How are you? ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    clock.classList.remove(HIDDEN_CLASSNAME);
 }
 
+function whatTimeIsIt() {
+    const date = new Date();
+    const hour = String(date.getHours()).padStart(2, '0');
+    const min = String(date.getMinutes()).padStart(2, '0');
+    const sec = String(date.getSeconds()).padStart(2, '0');
+    clock.innerHTML = `${hour}:${min}:${sec}`;
+}
+
+whatTimeIsIt();
+setInterval(whatTimeIsIt, 1000);
 loginForm.addEventListener("submit", onLogInSubmit);
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -27,5 +40,6 @@ if (savedUsername === null) {
     loginForm.addEventListener('submit', onLogInSubmit);
 } else {
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    question.classList.add(HIDDEN_CLASSNAME);
     paintGreetings(savedUsername);
 }
